b14025570345dc3ff22c4a8068b9ebdea67e4e12
raks <raksdaddy@outlook.com>
2024-11-28 19:40:53 +0000

    Update dump.js

--- discord/commands/dump.js
+++ discord/commands/dump.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const { dumprealm } = require('../../functions/bedrockrealms');
+const { dumprealm } = require('./functions/bedrockrealms');
 
 module.exports = {
     data: new SlashCommandBuilder()
