b6032a344b521eef3ae2eee9b449886a82d21b6d
raks <raksdaddy@outlook.com>
2024-11-28 19:40:11 +0000

    Add files via upload

---
+++ discord/commands/dump.js
@@ -0,0 +1,95 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { dumprealm } = require('../../functions/bedrockrealms');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('dump')
+        .setDescription('Get not much info on a realm.')
+        .setIntegrationTypes(0, 1)
+        .setContexts(0, 1, 2)
+        .addStringOption(option => 
+            option.setName('invite')
+                .setDescription('Realm invite code')
+                .setRequired(true)
+                .setMinLength(11)
+                .setMaxLength(15)),
+    async execute(interaction) {
+        const inviteCode = interaction.options.getString('invite');
+
+        try{
+
+        const embed = new EmbedBuilder()
+            .setTitle('expiring')
+            .setDescription(`Fetching realm data this may take a few seconds.`)
+
+            .setColor('#5865F2')
+            .setFooter({ text: 'expiring.lol'})
+            .setTimestamp(); 
+            await interaction.reply({ embeds: [embed] })
+        } catch(error){
+            if (error.code === 10008 || error.message.includes('Unknown Message')) {
+                return; 
+            } else {
+                console.log(error.message);
+            }
+        }
+        try {
+            const realmInfo = await dumprealm(inviteCode);
+
+            if (!realmInfo) {
+                try{
+
+                const embed = new EmbedBuilder()
+                .setTitle('expiring')
+                .setDescription(`Invalid code given. Please check if that is a valid code, **/checkcode**.`)
+                .setColor('#5865F2')
+                .setFooter({ text: 'expiring.lol'})
+                .setTimestamp();
+
+                return  interaction.editReply({ embeds: [embed] })
+            } catch(error){
+                if (error.code === 10008 || error.message.includes('Unknown Message')) {
+                    return; 
+                } else {
+                    console.log(error.message);
+                }
+            }
+            }
+
+            try{
+
+            const embed = new EmbedBuilder()
+                .setTitle('expiring')
+                .setDescription(`\`\`\`json\n${JSON.stringify(realmInfo, null, 2)}\n\`\`\``)
+                .setColor('#5865F2')
+                .setFooter({ text: 'expiring.lol'})
+                .setTimestamp();
+            await interaction.editReply({ embeds: [embed] });
+        } catch(error){
+            if (error.code === 10008 || error.message.includes('Unknown Message')) {
+                return; 
+            } else {
+                console.log(error.message);
+            }
+        }
+        } catch (error) {
+            try{
+
+            const embed = new EmbedBuilder()
+                .setTitle('expiring')
+                .setDescription(`\`\`\`json\n${JSON.stringify(error.message, null, 2)}\`\`\``)
+
+                .setColor('#5865F2')
+                .setFooter({ text: 'expiring.lol'})
+                .setTimestamp();  
+            return interaction.editReply({ embeds: [embed] })
+        } catch(error){
+            if (error.code === 10008 || error.message.includes('Unknown Message')) {
+                return; 
+            } else {
+                console.log(error.message);
+            }
+        }
+        }
+    },
+};
